7291e40d975c3726c58e1ef7cad6b54fa9293367
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-09-17 22:38:10 +0000

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: ShareBible(),
   ));
 }
@@ -12,6 +13,44 @@ class ShareBible extends StatefulWidget {
 }
 
 class _ShareBibleState extends State<ShareBible> {
+  final List<Map<String, dynamic>> bible = [
+    {
+      'book': 'Gen',
+      "chapterNumber": 1,
+      "verseNumber": 1,
+      "value": "In the beginning, God created the heavens and the earth.  "
+    },
+    {
+      'book': 'Gen',
+      "chapterNumber": 2,
+      "verseNumber": 1,
+      "value": "In the, God created the heavens and the earth.  "
+    },
+    {
+      'book': 'Exo',
+      "chapterNumber": 1,
+      "verseNumber": 1,
+      "value": "In the beginning, God created the heavens and the earth.  "
+    },
+    {
+      'book': 'Exo',
+      "chapterNumber": 2,
+      "verseNumber": 1,
+      "value": "In the, God created the heavens and the earth.  "
+    },
+    {
+      'book': 'Lev',
+      "chapterNumber": 1,
+      "verseNumber": 1,
+      "value": "In the beginning, God created the heavens and the earth.  "
+    },
+    {
+      'book': 'Lev',
+      "chapterNumber": 2,
+      "verseNumber": 1,
+      "value": "In the, God created the heavens and the earth.  "
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +58,27 @@ class _ShareBibleState extends State<ShareBible> {
       appBar: AppBar(
         title: const Text('ShareBible Json'),
         centerTitle: false,
-        backgroundColor: Colors.amber[800],
+        backgroundColor: Colors.teal,
         elevation: 0.0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 20.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                child: const Row(
+                  children: [Text('data')],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
